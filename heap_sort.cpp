ee2bce7c8d63e00b2e43c4ff7a70168bcd53e3c3
ivanzayda <60324391+ivanzayda@users.noreply.github.com>
2020-02-01 03:19:03 +0200

    Create heap_sort.cpp

---
+++ heap_sort.cpp
@@ -0,0 +1,35 @@
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+
+void heapSort(int arr[], int n){
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+        for (int i=n-1; i>=0; i--)
+            {
+                swap(arr[0], arr[i]);
+                heapify(arr, i, 0);
+             }
+}
+
+//usage:
+heapSort(arr,length);
+
+/*average speed of sorting 1 000 000 array (0-65000) is 0.436 sec
+Big O is:
+Best         | Avg      | Worst
+O(nlogn)     | O(nlogn) | O(nlogn)
+*/
